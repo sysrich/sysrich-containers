83945bf427d38e35a21255b350cc4df9541190a1
Richard Brown <rbrownccb@opensuse.org>
2018-07-12 11:44:39 +0200

    Initial commit. Initial jump host setup

---
+++ rootco-de-jump-host-image/config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#======================================
+# Functions...
+#--------------------------------------
+test -f /.kconfig && . /.kconfig
+test -f /.profile && . /.profile
+
+#======================================
+# Greeting...
+#--------------------------------------
+echo "Configure image: [$kiwi_iname]..."
+
+#======================================
+# Fix user permissions
+#--------------------------------------
+baseSetupUserPermissions
+
+exit 0
