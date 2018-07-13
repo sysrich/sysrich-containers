390e2e523044def4580e6685c58498f2ed1bd241
Richard Brown <rbrownccb@opensuse.org>
2018-07-13 12:41:50 +0200

    Monitor sshd in serial

--- rootco-de-jump-host-image/root/usr/local/bin/entrypoint.sh
+++ rootco-de-jump-host-image/root/usr/local/bin/entrypoint.sh
@@ -4,15 +4,20 @@ echo "DEBUG: Keygen"
 # TODO: Make keys persistant
 /usr/sbin/sshd-gen-keys-start
 
+echo "DEBUG: Trigger monitor"
+/usr/local/bin/monitor.sh &
+
 echo "DEBUG: SSHD start"
-# Not -D because daemonisation needed to run monitor below
 /usr/sbin/sshd
 
-# Wait 1 minute, then kill sshd if no one connected
+# Wait 1 minute, then kill sshd as soon as no one is connected
 echo "DEBUG: 1m sleep"
 sleep 1m
 
 echo "DEBUG: Login check"
-if [ $(who | wc -l) = 0 ]
-  then /sbin/killproc sshd
-fi
+until [ $(who | wc -l) = 0 ]; do
+  :
+done
+
+echo "DEBUG: Kill sshd"
+/sbin/killproc sshd
