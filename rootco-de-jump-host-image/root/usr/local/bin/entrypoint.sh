f37a46e041615176f06ed49290299691e085e433
Richard Brown <rbrownccb@opensuse.org>
2018-07-13 15:19:41 +0200

    Remove timestamps

--- rootco-de-jump-host-image/root/usr/local/bin/entrypoint.sh
+++ rootco-de-jump-host-image/root/usr/local/bin/entrypoint.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-echo "$(date -Iseconds). container START"
-echo "$(date -Iseconds). sshd-gen-keys-start"
+echo "container START"
+echo "sshd-gen-keys-start"
 /usr/sbin/sshd-gen-keys-start
 
-echo "$(date -Iseconds). sshd START"
+echo "sshd START"
 /usr/sbin/sshd
 
 # Wait 15 seconds, then kill sshd as soon as no one is connected
-echo "$(date -Iseconds). 15s wait for initial login"
+echo "15s wait for initial login"
 sleep 15s
 
-echo "$(date -Iseconds). login monitor"
+echo "login monitor"
 until [ $(who | wc -l) = 0 ]; do
   :
 done
 
-echo "$(date -Iseconds). no users logged in, kill sshd"
+echo "no users logged in, kill sshd"
 /sbin/killproc sshd
 
-echo "$(date -Iseconds). container END"
+echo "container END"
